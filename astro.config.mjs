5b0dd653e615f9dd10a0a3aa82cdf310ddd746c8
Lamine Diallo <bah07565@gmail.com>
2026-05-05 19:30:45 +0000

    feat(analytics): integrate Umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,29 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import umami from "@yeskunall/astro-umami";
+import { loadEnv } from "vite";
+
+const { PUBLIC_UMAMI_WEBSITE_ID } = loadEnv(
+  process.env.NODE_ENV ?? "production",
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://laminediallo.me",
+  integrations: PUBLIC_UMAMI_WEBSITE_ID
+    ? [
+        umami({
+          id: PUBLIC_UMAMI_WEBSITE_ID,
+          endpointUrl: "https://bip.laminediallo.me",
+          trackerScriptName: "bip.js",
+          domains: ["laminediallo.me"],
+          autotrack: true,
+        }),
+      ]
+    : [],
   fonts: [
     {
       name: "Inter",
